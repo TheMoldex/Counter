109742671b623b4bb0b95794a42835f132ddc088
Denis Stashkov <themoldex@gmail.com>
2023-06-15 09:05:03 +0300

    добавил функционал простого  счетчика, реализованный через UIButton  и UILabel

--- Counter/MyViewController.swift
+++ Counter/MyViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   private var count = 0
+    
+    @IBOutlet weak var counterValue: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
     
-
+    private func changeCounterValue() {
+        counterValue.text = "Значение счетчика: \(count)"
+    }
+    
+    @IBAction func buttonDidTape(_ sender: Any) {
+        count += 1
+        changeCounterValue()
+    }
+    
 }
 
