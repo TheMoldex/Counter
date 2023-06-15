285c3f9590a74878d7709b75b414bfa8663ecd4b
Denis Stashkov <themoldex@gmail.com>
2023-06-15 16:55:11 +0300

    Реализована усложненная задача(практикум спринт 3): добавлен функционал: уменьшения счетчика, сброса, истории изменения

--- Counter/MyViewController.swift
+++ Counter/MyViewController.swift
@@ -9,10 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-   private var count = 0
-    
-    @IBOutlet weak var counterValue: UILabel!
-    
+    private var count = 0
+    private var time = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +20,41 @@ class ViewController: UIViewController {
     private func changeCounterValue() {
         counterValue.text = "Значение счетчика: \(count)"
     }
-    
+
+  
+    @IBOutlet weak var counterValue: UILabel!
+
     @IBAction func buttonDidTape(_ sender: Any) {
         count += 1
         changeCounterValue()
+        historyOfChange.text += "\n\(time.timeFormat) значение изменено на +1"
     }
+    
+    @IBAction func decreaseCounterValue(_ sender: Any) {
+        if count > 0 {
+            count -= 1
+            counterValue.text = "Значение счетчика: \(count)"
+            historyOfChange.text += "\n\(time.timeFormat) значение изменено на -1"
+        }else {
+            historyOfChange.text += "\n\(time.timeFormat) попытка уменьшить значение счётчика ниже 0"
+        }
+    }
+    
+    @IBAction func resetValue(_ sender: UIButton) {
+        count = 0
+        counterValue.text = "Значение счетчика: \(count)"
+        historyOfChange.text += "\n\(time.timeFormat) значение сброшено"
+    }
+    
+    @IBOutlet weak var historyOfChange: UITextView!
     
 }
 
+
+extension Date {
+    var timeFormat: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMM yyyy HH:mm:ss"
+        return dateFormatter.string(from: self)
+    }
+}
