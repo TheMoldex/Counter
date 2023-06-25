6d27557310cf6be62f6308146a4d8a61b82a0257
Denis Stashkov <themoldex@gmail.com>
2023-06-25 10:37:25 +0300

    изменина структура кода, добавлена библиотека SwiftLint

--- Counter/MyViewController.swift
+++ Counter/MyViewController.swift
@@ -11,45 +11,41 @@ class ViewController: UIViewController {
 
     private var count = 0
     private var time = Date()
-    
+    @IBOutlet weak private var counterValue: UILabel!
+    @IBOutlet weak private var historyOfChangeTextView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
+
     private func changeCounterValue() {
         counterValue.text = "Значение счетчика: \(count)"
     }
 
-  
-    @IBOutlet weak var counterValue: UILabel!
-
-    @IBAction func buttonDidTape(_ sender: Any) {
+    @IBAction private func buttonDidTape(_ sender: Any) {
         count += 1
         changeCounterValue()
-        historyOfChange.text += "\n\(time.timeFormat) значение изменено на +1"
+        historyOfChangeTextView.text += "\n\(time.timeFormat) значение изменено на +1"
     }
-    
-    @IBAction func decreaseCounterValue(_ sender: Any) {
+
+    @IBAction private func decreaseCounterValue(_ sender: Any) {
         if count > 0 {
             count -= 1
             counterValue.text = "Значение счетчика: \(count)"
-            historyOfChange.text += "\n\(time.timeFormat) значение изменено на -1"
-        }else {
-            historyOfChange.text += "\n\(time.timeFormat) попытка уменьшить значение счётчика ниже 0"
+            historyOfChangeTextView.text += "\n\(time.timeFormat) значение изменено на -1"
+        } else {
+            historyOfChangeTextView.text += "\n\(time.timeFormat) попытка уменьшить значение счётчика ниже 0"
         }
     }
-    
-    @IBAction func resetValue(_ sender: UIButton) {
+
+    @IBAction private func resetValue(_ sender: UIButton) {
         count = 0
         counterValue.text = "Значение счетчика: \(count)"
-        historyOfChange.text += "\n\(time.timeFormat) значение сброшено"
+        historyOfChangeTextView.text += "\n\(time.timeFormat) значение сброшено"
     }
-    
-    @IBOutlet weak var historyOfChange: UITextView!
-    
-}
 
+}
 
 extension Date {
     var timeFormat: String {
